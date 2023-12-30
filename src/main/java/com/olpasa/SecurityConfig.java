44ead44bb7d0539a9787237f23dc6f3a4e2c76f7
VE1998 <olpasa@DESKTOP-27443C9.olpasa.local>
2023-12-30 10:16:44 -0500

    iniciando con spring security

---
+++ src/main/java/com/olpasa/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.olpasa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+}
