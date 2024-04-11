3055e0682ad399710fb0f9a825e3ff62bf5bd310
VE1998 <olpasa@DESKTOP-27443C9.olpasa.local>
2024-04-11 17:03:12 -0500

    comensando denuevo

---
+++ src/main/java/com/olpasa/config/SecurityConfig.java
@@ -0,0 +1,80 @@
+package com.olpasa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(ssesion -> ssesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.GET, "/bancos").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/bancos/{id_banco}").hasAuthority("1");
+
+                    http.anyRequest().denyAll();
+                })
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(null);
+        provider.setUserDetailsService(null);
+        return  provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("victor")
+                .password("1234")
+                .authorities("1", "2", "3")
+                .build());
+        userDetailsList.add(User.withUsername("juan")
+                .password("1234")
+                .authorities("1", "2", "3")
+                .build());
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
